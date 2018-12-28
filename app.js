f9d994c80bc7e1751117ad4c3ddc28b3eebf399d
Mondal10 <amitmondal111.am@gmail.com>
2018-12-28 20:40:26 +0530

    Passing HTML pages to the server

--- app.js
+++ app.js
@@ -166,3 +166,21 @@
 // server.listen(8080);
 // /* setting localhost:8080 as a port where request will be responded or in simple words server will be lsitening to the port 8080*/
 // console.log("started");
+
+/**
+ * Passing HTML pages to the server
+ */
+
+// const http = require('http');
+// const fs = require('fs');
+// const server = http.createServer((req,res) => {
+//     console.log(`URL of the page: ${req.url}`);
+//     /* 200 is status code, content-type can be : JSON,HTML,text, etc */
+    
+//     res.writeHead(200, {'Content-Type' : 'text/html'});
+//     let readStreamData = fs.createReadStream(__dirname + '/index.html');
+//     readStreamData.pipe(res);
+// });
+// server.listen(8080);
+// /* setting localhost:8080 as a port where request will be responded or in simple words server will be lsitening to the port 8080*/
+// console.log("started");
