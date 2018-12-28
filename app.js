fbbaee9c3ca1e06736e457af78b93aa447c88d5f
Mondal10 <amitmondal111.am@gmail.com>
2018-12-28 19:25:13 +0530

    Writing Stream Data in chunks

--- app.js
+++ app.js
@@ -134,15 +134,17 @@
 // console.log("started");
 
 /**
- * Reading Stream Data in chunks
+ * Reading and Writing Stream Data in chunks
  */
 
 // const fs = require('fs');
 
 // let readStreamData = fs.createReadStream(__dirname + '/sample.txt','utf8');
 // /* If we don't use utf8 it will return hex codes */
+// let writeStreamData = fs.createWriteStream(__dirname + '/sampleWrite.txt');
 
 // readStreamData.on('data', (chunk) => {
 //     console.log('new chunk recieved:::::::::::::');
-//     console.log(chunk);
+//     // console.log(chunk);
+//     writeStreamData.write(chunk);
 // });
