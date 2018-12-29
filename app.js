534efc41f08e507f2c8c8a1099295c8cff789698
Mondal10 <amitmondal111.am@gmail.com>
2018-12-29 12:06:46 +0530

    Passing JSON

--- app.js
+++ app.js
@@ -168,7 +168,7 @@
 // console.log("started");
 
 /**
- * Passing HTML pages to the server
+ * Passing HTML pages
  */
 
 // const http = require('http');
@@ -180,6 +180,29 @@
 //     res.writeHead(200, {'Content-Type' : 'text/html'});
 //     let readStreamData = fs.createReadStream(__dirname + '/index.html');
 //     readStreamData.pipe(res);
+// });
+// server.listen(8080);
+// /* setting localhost:8080 as a port where request will be responded or in simple words server will be lsitening to the port 8080*/
+// console.log("started");
+
+/**
+ * Passing JSON 
+ */
+
+// const http = require('http');
+// const fs = require('fs');
+// const server = http.createServer((req,res) => {
+// //     console.log(`URL of the page: ${req.url}`);
+
+//     /* 200 is status code, content-type can be : JSON,HTML,text, etc */
+//     res.writeHead(200, {'Content-Type' : 'application/json'});
+//     const myObj = {
+//         fName: "Amit",
+//         lName: "Mondal",
+//         age: 22
+//     };
+//     /* res.end(myObj); won't return the object as the parameter passed in res.end() must be a string or a buffer */
+//     res.end(JSON.stringify(myObj));
 // });
 // server.listen(8080);
 // /* setting localhost:8080 as a port where request will be responded or in simple words server will be lsitening to the port 8080*/
